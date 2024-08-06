9ff95c87cb5c6376c0e1814d84ee81196c8e8fd4
Lucas Palomo <devlpalomo@gmail.com>
2024-08-06 10:44:02 -0300

    init: :tada: initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-template-service
+
+go 1.22
+
+require github.com/open2b/scriggo v0.56.1 // indirect
---
+++ pkg/template/template.go
@@ -0,0 +1,59 @@
+package template
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/open2b/scriggo"
+	"github.com/open2b/scriggo/native"
+	"io"
+	"io/fs"
+	"os"
+)
+
+type Service struct {
+	templates map[string]*scriggo.Template
+	dir       fs.FS
+}
+
+func New(dir string) *Service {
+	return &Service{
+		templates: make(map[string]*scriggo.Template),
+		dir:       os.DirFS(dir),
+	}
+}
+
+func (svc *Service) Register(name string, path string) error {
+	tmpl, err := scriggo.BuildTemplate(
+		svc.dir,
+		path,
+		&scriggo.BuildOptions{
+			Globals: native.Declarations{
+				"ctx": make(map[string]any),
+			},
+		})
+
+	if err != nil {
+		return err
+	}
+
+	svc.templates[name] = tmpl
+	return nil
+}
+
+func (svc *Service) Render(name string, context map[string]any, writer io.Writer) error {
+	tmpl, ok := svc.templates[name]
+	if !ok {
+		return fmt.Errorf("template %s not found", name)
+	}
+	return tmpl.Run(writer, map[string]interface{}{"ctx": context}, nil)
+}
+
+func (svc *Service) RenderAsString(name string, context map[string]any) (string, error) {
+	var buf bytes.Buffer
+
+	err := svc.Render(name, context, &buf)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
